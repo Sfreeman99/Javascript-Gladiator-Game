c9b43371767a4975c0bbed016e0419c97b2407d1
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-10-12 15:12:25 -0500

    added test for Gladiator function

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,7 +1,30 @@
+const $ = require('jquery');
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function Gladiator() {
     this.health = 100;
     this.rage = 0;
-    this.attack = Math.floor(Math.random() * 25);
+    this.attackLow = Math.floor(Math.random() * 25);
+    this.attackHigh = Math.floor(
+        Math.random() * (25 - this.attackLow) + (this.attackLow + 1)
+    );
+    this.heal = function() {
+        this.health += 15;
+    };
+
+    this.attack = function attack(defender) {
+        const d = getRandomIntInclusive(this.attackLow, this.attackHigh);
+        defender.health -= d;
+        this.rage += 15;
+    };
+    this.isDead = function() {
+        if (this.health <= 0) {
+            return 'Dead';
+        }
+    };
 }
 
 function main() {
@@ -10,3 +33,4 @@ function main() {
 //________________________JQuery Below ___________________________________________________
 
 $(main);
+exports.Gladiator = Gladiator;
--- src/lib/core.test.js
+++ src/lib/core.test.js
@@ -1,10 +1,5 @@
 const core = require('./core');
 
-describe('foo', function() {
-    test('should return 5', function() {
-        expect(other.foo()).toEqual(5);
-    });
-});
 describe('Gladiator', function() {
     test('has 100 health', function() {
         expect(new core.Gladiator().health).toEqual(100);
@@ -12,7 +7,18 @@ describe('Gladiator', function() {
     test('has 0 rage at the beginning', function() {
         expect(new core.Gladiator().rage).toEqual(0);
     });
-    test('random attack power of 25 or below', function() {
-        expect(new core.Gladiator().attack).toBeLessThanOrEqual(25);
+    test("attack high is higher than it's attack low", function() {
+        var G = new core.Gladiator();
+        expect(G.attackLow).toBeLessThan(G.attackHigh);
+    });
+});
+describe('Gladiator Attack', function() {
+    test('Can do damage to another player', function() {
+        var P1 = new core.Gladiator();
+        var P2 = new core.Gladiator();
+        P1.attack(P2);
+        expect(P1.rage).toEqual(15);
+        expect(P2.health).toBeGreaterThanOrEqual(75);
+        expect(P2.health).toBeLessThanOrEqual(99);
     });
 });
