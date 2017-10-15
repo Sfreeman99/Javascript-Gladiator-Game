ba1e3a4b4d8fe18beb1569ae911bc716d55581e9
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-10-15 17:03:20 -0500

    [index.js] got the stats bar to show and optional buttons to show
    Next I want to make the buttons work

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,47 @@
 const $ = require('jquery');
 const core = require('../../lib/core');
-
-function main() {
-    return 'foo';
+function turn() {
+    var turnBool = true;
+    if (turnBool === true) {
+    }
 }
+function main() {
+    $('#submit').click(function() {
+        const Player1 = new core.Gladiator($('#Player1').val());
+        const Player2 = new core.Gladiator($('#Player2').val());
+        $('#app').html(view(Player1, Player2));
+    });
+    // $('#app').html(view(Player1, Player2));
+}
+
+function view(attacker, defender) {
+    return [
+        '<div row>',
+        "<div class='col-lg-6' id='Player1'><h3>",
+        'Gladiator Name: &nbsp;' +
+            attacker.name +
+            '|| &nbsp;Health: ' +
+            attacker.health +
+            '|| &nbsp; Attack Low: ' +
+            attacker.attackLow +
+            '|| &nbsp; Attack High: ' +
+            attacker.attackHigh +
+            '</h3></div>',
+        "<div class='col-lg-6' id='Player2'><h3>",
+        'Gladiator Name: &nbsp;' +
+            defender.name +
+            '|| &nbsp;Health: ' +
+            defender.health +
+            '|| &nbsp; Attack Low: ' +
+            defender.attackLow +
+            '|| &nbsp; Attack High: ' +
+            defender.attackHigh +
+            '</h3></div>',
+        "<div row><div class='col-lg-12' id='moves'>",
+        attacker.name +
+            " what do you want to do: &nbsp;<button class='btn btn-danger' id='attack'> Attack </button>",
+        "<button class='btn btn-danger' id='heal'> Heal </button>"
+    ].join(' ');
+}
+
 $(main);
